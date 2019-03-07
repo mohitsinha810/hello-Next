ad1a16ac1472aac5fae6bd5183c5d1bae710e7e2
Mohit <mohitsinha810@gmail.com>
2019-03-08 00:34:46 +0545

    Created basic layout

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+import Layout from '../components/MyLayout'
+
+export default () => (
+	<Layout>
+		<p>This is the about page.</p>
+	</Layout>
+	);
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Layout from '../components/MyLayout'
+
+const Index = () => (
+	<Layout>
+		<p>This is the home page.</p>
+	</Layout>
+	);
+
+export default Index;
